349cb220c8c1fe3d8f4bc599cb8859d482e2fe59
Mathieu Ponce <poncemathieu@gmail.com>
2021-06-18 14:39:53 +0200

    first commit

--- Eagle.java
+++ Eagle.java
@@ -1,24 +1,81 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
-    private boolean flying;
-    private int altitude;
+	private boolean flying;
+	private int altitude;
 
-    public Eagle(String name) {
-        super(name);
-        this.flying = false;
-        this.altitude = 0;
-    }
+	public Eagle(String name) {
+		super(name);
+		this.flying = false;
+		this.altitude = 0;
+	}
 
-    public int getAltitude() {
-        return altitude;
-    }
+	public int getAltitude() {
+		return altitude;
+	}
 
-    public boolean isFlying() {
-        return flying;
-    }
+	public void setAltitude(int altitude) {
+		this.altitude = altitude;
+	}
 
-    @Override
-    public String sing() {
-        return "Screech!";
-    }
+	public boolean isFlying() {
+		return flying;
+	}
+
+	public void setFlying(boolean flying) {
+		this.flying = flying;
+	}
+
+	@Override
+	public String sing() {
+		return "Screech!";
+	}
+
+	@Override
+	public void takeOff() {
+		if (!this.isFlying() && this.getAltitude() == 0) {
+			this.setFlying(true);
+			System.out.println(this.getName() + " takes off in the sky");
+		} else
+			System.out.println(this.getName() + " is already flying");
+	}
+
+	@Override
+	public void ascend(int altitude) {
+		if (this.flying) {
+			this.setAltitude(this.getAltitude() + altitude);
+			System.out.println(this.getName() + " flies upward, altitude: " + this.getAltitude());
+		} else
+			System.out.println(this.getName() + " needs to fly first");
+
+	}
+
+	@Override
+	public void glide() {
+		if (this.flying) {
+			System.out.println(this.getName() + " glides in the air");
+		} else
+			System.out.println(this.getName() + " needs to fly first");
+	}
+
+	@Override
+	public void descend(int altitude) {
+		if (this.flying) {
+			if (this.getAltitude() - altitude <= 0) {
+				this.setAltitude(this.getAltitude() - altitude);
+				System.out.println(this.getName() + " descends, altitude: " + this.getAltitude());
+			} else
+				System.out.println(this.getName() + " cannot descend below the ground");
+		} else
+			System.out.println(this.getName() + " needs to fly firs");
+	}
+
+	@Override
+	public void land() {
+		if (this.flying) {
+			this.setAltitude(0);
+			this.flying = false;
+			System.out.println(this.getName() + " lands");
+		} else
+			System.out.println(this.getName() + " is not flying");
+	}
 }
---
+++ Fly.java
@@ -0,0 +1,8 @@
+
+public interface Fly {
+	void takeOff();
+	void ascend(int altitude);
+	void glide();
+	void descend(int altitude);
+	void land();
+}
